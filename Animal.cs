b2c5255ec7df1619eed4d60cd88eb244b520558f
Yaroslav <yaroslav.kerez@gmail.com>
2024-07-31 18:41:19 +0200

    Add project files.

---
+++ Animal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS_8_Interfaces
+{
+    internal class Animal
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Put dog name");
+        string name = Console.ReadLine();
+        Animal Dog = new Dog();
+        Dog.SetName(name);
+        Dog.Eat();
+    }
+
+    public abstract class Animal
+    {
+        private string _name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public abstract void Eat();
+
+        public void SetName(string name)
+        {
+            _name = name;
+        }
+
+        public string GetName()
+        {
+            return _name.ToString();
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public override void Eat()
+        {
+            Console.WriteLine($"Dog have a name {0} eat food");
+        }
+    }
+}
